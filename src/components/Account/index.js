965049455e52f51b0f878978e388ee3e82c37fe7
Alla Chebotarska <alla.chebotarska@gmail.com>
2021-02-23 15:09:28 -0800

    Added react router for firebase auth

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
- 
-const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
-);
- 
-export default App;
+import React from 'react'
+
+export default function Account() {
+  return (
+    <div>
+      <h1>Account</h1>
+    </div>
+  )
+}
