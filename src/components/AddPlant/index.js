76e8995ef9b66accafcb56ff497ba8eeca5a5c47
Alla Chebotarska <alla.chebotarska@gmail.com>
2021-03-02 14:32:07 -0800

    Watering/Spraing togge

--- src/components/AddPlant/index.js
+++ src/components/AddPlant/index.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import CareType from '../CareType'
+import CareType from '../CareType';
+import Divider from '@material-ui/core/Divider';
 
-export default function AddPlant() {
-    const [checked, setChecked] = React.useState(false);
+export default class AddPlant extends Component {
 
-    const toggleChecked = () => {
-        setChecked((prev) => !prev);
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            watering: false,
+            spraying: false,
+        }
+    }
 
-    return (
-        <div>
-            <h3>Add Plant</h3>
-            <form>
-                <TextField id="standard-basic" label="Plant name" />
-                <br />
-                <h5>Watering</h5>
-                <FormControlLabel
-                    control={<Switch checked={checked} onChange={toggleChecked} />}
-                />
-                <CareType />
-                
-                <input type="submit" value="Save" />
-            </form>
-        </div>
-    )
+    wateringChecked = () => {
+        let previousWateringState = this.state.watering;
+        this.setState({
+            watering: !previousWateringState,
+        });
+    }
+
+    sprayingChecked = () => {
+        let previousWateringState = this.state.spraying;
+        this.setState({
+            spraying: !previousWateringState,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Add Plant</h3>
+                <form>
+                    <TextField id="standard-basic" label="Plant name" />
+                    <br />
+                    <h5>Watering</h5>
+                    <FormControlLabel
+                        control={<Switch onChange={() => this.wateringChecked()} />}
+                    />
+                    {this.state.watering === true ? <CareType /> : <div></div>}
+
+                    <h5>Spraying</h5>
+                    <FormControlLabel
+                        control={<Switch onChange={() => this.sprayingChecked()} />}
+                    />
+                    {this.state.spraying === true ? <CareType /> : <div></div>}
+                    <Divider />
+                    <input type="submit" value="Save" />
+                </form>
+            </div>
+        )
+    }
+
 }
+
+
+
