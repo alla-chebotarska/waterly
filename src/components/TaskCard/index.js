366c77c395dad52964e692847159ee9799d97952
Alla Chebotarska <alla.chebotarska@gmail.com>
2021-03-30 12:55:18 -0700

    To-Do list Page style in progress

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -5,19 +5,19 @@ import drop from '../../icons/drop.svg';
 import shovel from '../../icons/shovel.svg';
 import './taskCard.css';
 
-export default function TaskCard({care, plant}) {
+export default function TaskCard({ care, plant }) {
 
     let lastCare = new Date(care.lastCare).toDateString();
 
     const choseCareIcon = (careType) => {
-        let careIcon = drop;
-        switch(careType){
+        let careIcon = shovel;
+        switch (careType) {
             case "Watering": careIcon = drop;
-            break;
+                break;
             case "Spraying": careIcon = sprayer;
-            break;
+                break;
             case "Fertilizing": careIcon = shovel;
-            break;
+                break;
         }
         return careIcon;
     }
@@ -25,13 +25,16 @@ export default function TaskCard({care, plant}) {
     return (
         <div className="task-card-container">
             <div className='plant-image-container'>
-                <PlantImage 
+                <PlantImage
                     plantId={plant.plantIconId}
                 />
             </div>
             <div className='task-information'>
-                <p>{care.careType}</p>
-                <p>Last: {lastCare}</p>
+                <div style={{overflow: "hidden"}}>
+                    <p className="care-information">{care.careType}</p>
+                    <p className="care-information plant-name"> {plant.name}</p>
+                </div>
+                <p className="grey-text">Last: {lastCare}</p>
             </div>
             <div className='care-icon-container'>
                 <img src={choseCareIcon(care.careType)} className='care-type-icon' alt='care icon' />
