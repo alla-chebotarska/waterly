cdeab3c4a4363493ab90ad10d94b7749a844a8c0
Alla Chebotarska <alla.chebotarska@gmail.com>
2021-04-01 12:07:31 -0700

    Styles were added to Login page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from 'react';
+import GoogleButton from 'react-google-button';
+import { FacebookProvider, LoginButton } from 'react-facebook';
 import { FirebaseContext } from '../FireBase';
+import leaf from '../../icons/leaf.svg';
+import './singIn.css';
 
 export default function SignIn() {
     return (
         <div>
-            <p>Logo</p>
-            <FirebaseContext.Consumer>
-                {firebase => <button onClick={() => firebase.signInWithGoogle()}>Sign in with Google</button>}
-            </FirebaseContext.Consumer>
-            <br/>
-            <button>Sign in with FaceBook</button>
+            <div className="waterly-logo-container">
+                <img src={leaf} alt="waterly logo" className="waterly-logo" />
+            </div>
+            <h1 className="waterly-logo-text">Waterly</h1>
+            <div className="signin-buttons-container">
+                <div className="google-signin">
+                    <FirebaseContext.Consumer>
+                        {firebase => <GoogleButton onClick={() => firebase.signInWithGoogle()}>Sign in with Google</GoogleButton>}
+                    </FirebaseContext.Consumer>
+                </div>
+                {/* <FacebookProvider appId="123456789">
+                    <LoginButton>
+                        <span>Login via Facebook</span>
+                    </LoginButton>
+                </FacebookProvider> */}
+            </div>
+
         </div>
     )
 }
