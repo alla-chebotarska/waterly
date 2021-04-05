d5ea9506a91d54bbda53c10f814667993b12358b
Alla Chebotarska <alla.chebotarska@gmail.com>
2021-04-05 16:02:14 -0700

    Color for CareType toggle was changed to green

--- src/components/ToggleButton/index.js
+++ src/components/ToggleButton/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 export default function ToggleButton(props) {
+    const MySwitch = withStyles({
+        switchBase: {
+          color: 'FFFFFF',
+          '&$checked': {
+            color: '#1FC49F',
+          },
+          '&$checked + $track': {
+            backgroundColor: "#2becc2",
+          },
+        },
+        checked: {},
+        track: {},
+      })(Switch);
     return (
         <div>
             <FormControlLabel
             label={props.title}
             labelPlacement="start"
-                control={<Switch
+                control={<MySwitch
                     checked={props.checked}
                     onChange={(event) => { props.onChange(event.target.checked) }} />}
             />
